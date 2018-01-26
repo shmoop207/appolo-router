1562f441aae4ecae5dff9d5ceca7b290f187197f
roman <shmoop207@gmail.com>
2018-01-26 15:38:13 +0200

    first

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var router_1 = require("./lib/router");
 exports.Router = router_1.Router;
+var enums_1 = require("./lib/enums");
+exports.Methods = enums_1.Methods;
 //# sourceMappingURL=index.js.map
